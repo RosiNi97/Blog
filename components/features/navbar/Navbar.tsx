75a94a7899ace56bf1741cf7ca51822dd280af32
Nikolay <nikolai.nikolaev.kamenov@gmail.com>
2022-11-21 18:16:24 +0200

    redux added

---
+++ components/features/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+    </div>
+  );
+};
+
+export default Navbar;
