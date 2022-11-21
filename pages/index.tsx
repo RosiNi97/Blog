1d6062976701d669b357c6956ffb4d0b9c039b27
Nikolay <nikolai.nikolaev.kamenov@gmail.com>
2022-11-21 19:33:15 +0200

    removed Redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
+import Navbar from "../components/features/navbar/Navbar";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <title>Blog</title>
         <meta name="blog" content="blog" />
       </Head>
+      <Navbar />
     </div>
   );
 }
