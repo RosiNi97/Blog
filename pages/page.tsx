d3f0cdb9533836ac22ac13dd5b645281dafef71c
RosiNi97 <109990062+RosiNi97@users.noreply.github.com>
2022-11-26 13:55:27 +0200

    pulling navbar

---
+++ pages/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+    <div className="Page">
+        <h1>Title</h1>
+        <textarea id="w3review" name="w3review" rows= {15} cols={45}>
+</textarea>
+
+         <input type="button" id="add" value="add"/>
+    </div>
+    )
+}
