99428cb4adc1bd5a2d4ec0bdd1f0ce2bfe9b24de
Nikolay <nikolai.nikolaev.kamenov@gmail.com>
2022-12-12 20:33:56 +0200

    articles undefined bugfix

--- components/features/profile/UserPosts.tsx
+++ components/features/profile/UserPosts.tsx
@@ -2,19 +2,19 @@ import { doc, DocumentData, onSnapshot } from "firebase/firestore";
 import Link from "next/link";
 import { createContext, useEffect, useState } from "react";
 import auth from "../../../firebase/auth";
-import db, { currentUserArticles } from "../../../firebase/firestore";
+import db from "../../../firebase/firestore";
 import ArticleTemplate from "./articles/ArticleTemplate";
 
 export const ArticleContext = createContext({ articleList: [{}] });
 
 const UserPostList = () => {
-  const [articleList, setArticleList] = useState([]);
+  const [articleList, setArticleList] = useState([{}]);
 
   useEffect(() => {
     onSnapshot(doc(db, "articles", auth.currentUser?.uid as string), (doc) => {
       if (doc !== undefined) {
         const docData = doc.data();
-        setArticleList(docData?.articles);
+        setArticleList(docData?.articles as Array<object>);
       }
       return {};
     });
--- components/features/profile/articles/ArticleTemplate.tsx
+++ components/features/profile/articles/ArticleTemplate.tsx
@@ -1,17 +1,27 @@
 const ArticleTemplate = (props: any) => {
   const articleList = props.articleList;
 
-  return (
-    <div>
-      {articleList.map((article: any, key: number) => {
+  if (articleList !== undefined) {
+    articleList.map((article: any, key: number) => {
+      return (
+        <div key={key}>
+          <h3>Title : {article.title}</h3>
+          <p>{article.contents}</p>
+        </div>
+      );
+    });
+  }
+  return <div>No Articles To Show</div>;
+
+  {
+    /* {articleList.map((article: any, key: number) => {
         return (
           <div key={key}>
             <h3>Title : {article.title}</h3>
             <p>{article.contents}</p>
           </div>
         );
-      })}
-    </div>
-  );
+      })} */
+  }
 };
 export default ArticleTemplate;
