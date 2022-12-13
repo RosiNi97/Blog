3626d1046f68ef8f508330ba6f2cf1f661297dce
Nikolay <nikolai.nikolaev.kamenov@gmail.com>
2022-12-13 15:46:46 +0200

    Working Now With no Articles

--- components/features/profile/articles/ArticleTemplate.tsx
+++ components/features/profile/articles/ArticleTemplate.tsx
@@ -1,30 +1,21 @@
 const ArticleTemplate = (props: any) => {
   const articleList = props.articleList;
 
-  // const showArticles = () => {
-  //   if (articleList != undefined) {
-  //     articleList.map((article: any, key: number) => {
-  //       return (
-  //         <div key={key}>
-  //           <h3>Title : {article.title}</h3>
-  //           <p>{article.contents}</p>
-  //         </div>
-  //       );
-  //     });
-  //   } else return <div>No Articles To Show</div>;
-  // };
-
-  return (
-    <div>
-      {articleList.map((article: any, key: number) => {
-        return (
-          <div key={key}>
-            <h3>Title : {article.title}</h3>
-            <p>{article.contents}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (articleList !== undefined) {
+    return (
+      <div>
+        {articleList.map((article: any, key: number) => {
+          return (
+            <div key={key}>
+              <h3>Title : {article.title}</h3>
+              <p>{article.contents}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return <div>No articles to show</div>;
+  }
 };
 export default ArticleTemplate;
