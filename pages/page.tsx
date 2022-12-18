eff2a036025824c189aef5569db2dca9ba99eaec
Gregari Iliev <Garson_2006@hotmail.com>
2022-12-18 17:52:21 +0200

    format code

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,11 +1,10 @@
 export default function Page() {
-    return (
+  return (
     <div className="Page">
-        <h1>Title</h1>
-        <textarea id="w3review" name="w3review" rows= {15} cols={45}>
-</textarea>
+      <h1>Title</h1>
+      <textarea id="w3review" name="w3review" rows={15} cols={45}></textarea>
 
-         <input type="button" id="add" value="add"/>
+      <input type="button" id="add" value="add" />
     </div>
-    )
+  );
 }
