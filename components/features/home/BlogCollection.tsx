ad2c632fc396aa369380da98a3dda55d43e80d1c
RosiNi97 <109990062+RosiNi97@users.noreply.github.com>
2022-12-20 19:27:22 +0200

    Update BlogCollection.tsx

--- components/features/home/BlogCollection.tsx
+++ components/features/home/BlogCollection.tsx
@@ -1,6 +1,6 @@
 import { getBlogCollections } from "../../../firebase/firestore";
 import { useEffect, useState } from "react";
-import styles from "../../styles/Blog.module.css";
+import styles from "../../styles/blog.module.css";
 import { DocumentData, QuerySnapshot } from "firebase/firestore";
 
 export default function BlogCollection() {
@@ -20,11 +20,26 @@ export default function BlogCollection() {
     <div>
       {blogs
         ? blogs.map((b) => (
-            <div key={b.title}>
-              <h1>{b.title}</h1>
-              <div>{b.video}</div>
-            </div>
-          ))
+          <div key={b.title}>
+            <h1>{b.title}</h1>
+            <div>{b.video}</div>
+            <div className={styles.blogmodule}>
+
+            <section className={styles.img}>
+              <img src="https://t3.ftcdn.net/jpg/02/51/30/52/360_F_251305284_M7NOdeDXcXx44WkUWkHQijztn3yneroq.jpg" alt="working"/>
+            </section>
+            <aside className={styles.details}>
+              <details >
+                <summary>More</summary>
+                <p></p>
+              </details>
+              <details>
+                <summary>Social media</summary>
+              </details>
+            </aside>
+          </div>
+          </div>
+        ))
         : null}
     </div>
   );
