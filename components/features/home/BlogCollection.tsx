ade48a7d9b6570a1fa76cea8aa1caebb9aeae65b
RosiNi97 <109990062+RosiNi97@users.noreply.github.com>
2022-12-21 08:47:21 +0200

    blogcollection

--- components/features/home/BlogCollection.tsx
+++ components/features/home/BlogCollection.tsx
@@ -20,22 +20,40 @@ export default function BlogCollection() {
       setArticleList(blogList);
     });
   }, []);
-
   return (
     <div>
-      {articleList
-        ? articleList.map((b) => (
-            <div key={b.id + b.title}>
-              <h3>Title : {b.title}</h3>
-              <p>{b.contents}</p>
-              <iframe
-                width="853"
-                height="480"
-                src={`https://www.youtube.com/embed/${b.videoID}`}
-                allow="accelerometer;autoplay; clipboard-write;encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title={b.title}
-              />
+      {blogs
+        ? blogs.map((b) => (
+            <div key={b.title}>
+              <h1>{b.title}</h1>
+              <div>{b.video}</div>
+              <div key={b.id}>
+                <h3>Title : {b.title}</h3>
+                <p>{b.contents}</p>
+                <section>
+                  <img
+                    src="https://t3.ftcdn.net/jpg/02/51/30/52/360_F_251305284_M7NOdeDXcXx44WkUWkHQijztn3yneroq.jpg"
+                    alt="working"
+                  />
+                </section>
+                <aside>
+                  <details>
+                    <summary>More</summary>
+                    <p></p>
+                  </details>
+                  <details>
+                    <summary>Social media</summary>
+                  </details>
+                </aside>
+                <iframe
+                  width="853"
+                  height="480"
+                  src={`https://www.youtube.com/embed/${b.videoID}`}
+                  allow="accelerometer;autoplay; clipboard-write;encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title={b.title}
+                />
+              </div>
             </div>
           ))
         : "No Content To Show"}
